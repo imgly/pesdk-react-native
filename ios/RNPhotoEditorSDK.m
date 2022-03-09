ddc8b9c147a6a211f6bd9ae18caabf49abe9ef9f
foloinfo <yoshiaki.tsutsumi@byus.me>
2022-03-10 08:37:45 +0900

    Update RNPhotoEditorSDK.m
    ADD localization for test

--- ios/RNPhotoEditorSDK.m
+++ ios/RNPhotoEditorSDK.m
@@ -35,6 +35,15 @@ static RNPESDKWillPresentBlock _willPresentPhotoEditViewController = nil;
 - (void)present:(nullable PESDKPhoto *)photo withConfiguration:(nullable NSDictionary *)dictionary andSerialization:(nullable NSDictionary *)state
         resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject
 {
+
+  [PESDKPhoto setLocalizationDictionary: @{
+    @"ja": @{
+      @"pesdk_transform_title_name": @"変換",
+      @"pesdk_adjustments_title_name": @"調整",
+      @"pesdk_adjustments_button_reset": @"取り消し"
+    }
+  }];
+
   [self present:^PESDKMediaEditViewController * _Nullable(PESDKConfiguration * _Nonnull configuration, NSData * _Nullable serializationData) {
 
     PESDKPhoto *photoAsset = photo;
